d004aa50da93955e9165798a8eedf0cfc5e901ce
daivengalan <daivenclark@gmail.com>
2026-03-04 19:17:27 +0800

    sets up an mcp using arcade, now uses the test_api as temp. instead of paracetamol as variable, will now use drug name for more incusive variable

---
+++ side_effects_mcp/src/side_effects_mcp/server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""side_effects_mcp MCP server"""
+
+import sys
+from typing import Annotated
+
+import requests
+from arcade_mcp_server import MCPApp
+
+app = MCPApp(name="side_effects_mcp", version="1.0.0", log_level="DEBUG")
+
+@app.tool
+def get_side_effects_for_drug(drug_name: Annotated[str, "The name of the medical drug to find side effects for"]) -> list[dict]:
+    base_url = 'https://clinicaltrials.gov/api/v2/studies'
+
+    params = {
+        'query.term': 'paracetamol',
+        'pageSize': 25,
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code != 200:
+        print(f"Error! Status code: {response.status_code}")
+        print("The server returned HTML instead of JSON. Check your URL.")
+    else:
+        data = response.json()
+        print(data)
+
+        aggregated = {}
+        studies = data.get('studies', [])
+
+        for study in studies:
+            if study.get('hasResults'):
+                adverse = study.get('resultsSection', {}).get('adverseEventsModule', {})
+
+                if 'seriousEvents' in adverse:
+                    events = adverse['seriousEvents']
+
+                    for event in events:
+                        stats = event.get('stats', [])
+                        if stats and stats[0].get('numAtRisk', 0) > 0:
+                            name = event.get('term')
+                            probability = stats[0]['numAffected'] / stats[0]['numAtRisk']
+
+                            if name in aggregated:
+                                aggregated[name].append(probability)
+                            else:
+                                aggregated[name] = [probability]
+
+        results = [
+            {
+                'side_effect_name': name,
+                'side_effect_probability': sum(probabilities) / len(probabilities)
+            }
+            for name, probabilities in aggregated.items()
+        ]
+
+        print([side_effect for side_effect in results if side_effect['side_effect_probability'] > 0.01])
+if __name__ == "__main__":
+   transport = sys.argv[1] if len(sys.argv) > 1 else "stdio"
+
+    # Run the server
+    app.run(transport=transport, host="127.0.0.1", port=8000)
